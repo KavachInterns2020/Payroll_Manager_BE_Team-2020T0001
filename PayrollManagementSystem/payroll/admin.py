d84b72712c3ff530e3f3c9ac607ad4bdc6ef43ba
Himanshu Mishra <59360424+dev-himanshu@users.noreply.github.com>
2020-07-08 12:31:39 +0530

    Autogenerate company-id

--- PayrollManagementSystem/payroll/admin.py
+++ PayrollManagementSystem/payroll/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Companies, AdminUser
 
+# admin credentials
+# username -> admin
+# email -> admin@payrollmanager.com
+# password -> django
 
 # Register your models here.
 admin.site.register(Companies)
--- PayrollManagementSystem/payroll/models.py
+++ PayrollManagementSystem/payroll/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Companies(models.Model):
-    companyId = models.CharField(max_length=100, primary_key=True, unique=True)
     companyName = models.CharField(max_length=250)
     companyAddress = models.CharField(max_length=250)
     companyEmailAddress = models.EmailField(max_length=250, unique=True)
@@ -21,7 +20,7 @@ class Companies(models.Model):
 
 class AdminUser(models.Model):
     adminId = models.CharField(max_length=150, primary_key=True, unique=True)
-    companyId = models.CharField(max_length=100)
+    companyId = models.ForeignKey(Companies, default=None, on_delete=models.CASCADE)
     adminName = models.CharField(max_length=250)
     Password = models.CharField(max_length=250)
 
