bfe6c45734dd6487471901d768966994a21ec9a8
Himanshu Mishra <59360424+dev-himanshu@users.noreply.github.com>
2020-07-09 19:33:48 +0530

    changes

--- PayrollManagementSystem/companyRegistrationAndLoginApplication/views.py
+++ PayrollManagementSystem/companyRegistrationAndLoginApplication/views.py
@@ -5,47 +5,37 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 
+
 # Create your views here.
 class LoginView(APIView):
-    def post(self,request):
+    def post(self, request):
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
         login(request,user)
         token , created =Token.objects.get_or_create(user=user)
-        return Response({"token":token.key},status=200)
+        return Response({"token": token.key}, status=200)
+
 
 class LogoutView(APIView):
     authentication_classes = (TokenAuthentication, )
 
-    def post(self ,request):
+    def post(self, request):
         return Response(status=204)
 
 # this is user defined method
-
-#perform makemigrations
-#perform migrate
-
-#login
+# perform makemigrations
+# perform migrate
+# login
 # api --->  http://localhost:8080/auth/login/
-
-
-#logout
+# logout
 # api --->  http://localhost:8080/auth/logout/
-
-
-
-
 # for using default method----
-
-
 # type  in cmd- pip install djoser
-
-#api ----> http://localhost:8080/auth/token/login/
-
-#api ----> http://localhost:8080/auth/token/logout/
+# api ----> http://localhost:8080/auth/token/login/
+# api ----> http://localhost:8080/auth/token/logout/
