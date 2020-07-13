f4357bd6e85f7cdb06ced1972b3aef03cfa4d770
Himanshu Mishra <59360424+dev-himanshu@users.noreply.github.com>
2020-07-14 02:19:02 +0530

    ...

--- PayrollManagementSystem/companyRegistrationAndLoginApplication/serializers.py
+++ PayrollManagementSystem/companyRegistrationAndLoginApplication/serializers.py
@@ -46,28 +46,28 @@ class CompanySerializer(serializers.ModelSerializer):
         admin_model.save()
         return companies
 
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
-    def validate(self,data):
-        username = data.get("username","")
-        password = data.get("password","")
+    def validate(self, data):
+        username = data.get("username", "")
+        password = data.get("password", "")
 
-        if username and password :
+        if username and password:
             user = authenticate(username=username,password=password)
             if user:
                 if user.is_active:
                     data['user'] = user
                 else:
-                    msg="User is Not Found"
+                    msg = "User is Not Found"
                     raise exceptions.ValidationError(msg)
             else:
-                msg="Wrong Credentials"
+                msg = "Wrong Credentials"
                 raise exceptions.ValidationError(msg)
         else:
-            msg="Must Provide username and Password"
+            msg = "Must Provide username and Password"
             raise exceptions.ValidationError(msg)
-        
-
         return data
+
