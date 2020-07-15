09fee6c59848eacbc5d55c6c14408b086c31a45b
Sulabh Raj <pandeysulabhraj97@gmail.com>
2020-07-15 18:08:28 +0530

    update

--- PayrollManagementSystem/companyRegistrationAndLoginApplication/urls.py
+++ PayrollManagementSystem/companyRegistrationAndLoginApplication/urls.py
@@ -4,6 +4,5 @@ from .views import LoginView,LogoutView
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('', include('djoser.urls')),
-    path('', include('djoser.urls.authtoken')),
+    
 ]
